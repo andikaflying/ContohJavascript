6722b023b0d79ad9466806d73420ac01c533c485
andikaflying <44762285+andikaflying@users.noreply.github.com>
2023-01-12 19:53:26 +0700

    updated

--- materi_4/four_2.js
+++ materi_4/four_2.js
@@ -18,10 +18,6 @@ document.write(deret.indexOf(12)  + "<br />");
 const welcome = "Selamat Jalan";
 document.write(welcome.indexOf("a")  + "<br />");
 
-function displayAlert() {
-    alert("Hello");
-}
-
 class Deret {
 
     constructor(deret) {
@@ -40,4 +36,27 @@ class Deret {
 const deretPertama = new Deret(deret);
 document.write(deretPertama.dapatIndex(11) + "<br />");
 document.write(deretPertama.cekApakahAda(20) + "<br />");
-displayAlert();
+
+const books = [
+    { title: 'The Da Vinci Code', author: 'Dan Brown', sales: 5094805 },
+    { title: 'The Ghost', author: 'Robert Harris', sales: 807311 },
+    { title: 'White Teeth', author: 'Zadie Smith', sales: 815586 },
+    { title: 'Fifty Shades of Grey', author: 'E. L. James', sales: 3758936 },
+    { title: 'Jamie\'s Italy', author: 'Jamie Oliver', sales: 906968 },
+    { title: 'I Can Make You Thin', author: 'Paul McKenna', sales: 905086 },
+    { title: 'Harry Potter and the Deathly Hallows', author: 'J.K Rowling', sales: 4475152 },
+];
+
+//Ingin mendapatkan list Author yg sales nya di atas 1000000
+const filteredBooks = books.filter((buku) => buku.sales > 1000000);
+document.write(JSON.stringify(filteredBooks)  + "<br />");
+const authorList = filteredBooks.map((item) => item.author);
+document.write(authorList);
+
+const helloAlert = () => alert("Hello");
+const haiAlert = () => {
+    alert("Hai");
+};
+
+helloAlert();
+haiAlert();
